7d3abd395c071149842565eed3dae326caeee0a4
Timo <Timo@DESKTOP-UH919KQ>
2020-10-24 18:11:53 +0200

    test commit

---
+++ EGH-git/src/test.java
@@ -0,0 +1,15 @@
+
+public class test {
+	
+
+	public static void main(String[] args) {
+		
+		double test = 123;
+		System.out.println(test);
+	
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
